843ac7e94c9bf9c61fa6949cc7417e62d523aead
timniklas <accounts@tnkoch.de>
2025-07-02 16:09:58 +0200

    Update __init__.py

--- custom_components/growatt_ble/__init__.py
+++ custom_components/growatt_ble/__init__.py
@@ -3,12 +3,10 @@ from .const import DOMAIN
 async def async_setup_entry(hass, entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
