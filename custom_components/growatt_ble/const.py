849427fc20a7f48af36d88bfeaa97532e11e81e8
timniklas <accounts@tnkoch.de>
2025-07-02 15:58:17 +0200

    Create const.py

---
+++ custom_components/growatt_ble/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "growatt_ble"
+
+REGISTERS = {
+    "output_power": {"reg": 2,  "scale": 0.25,  "unit": "W"},
+    "pv1_voltage":  {"reg": 5,  "scale": 0.01,  "unit": "V"},
+    "pv1_current":  {"reg": 6,  "scale": 0.005, "unit": "A"},
+    "pv2_voltage":  {"reg": 7,  "scale": 0.1,  "unit": "V"},
+    "pv2_current":  {"reg": 8,  "scale": 0.005,  "unit": "A"},
+    "pv1_power":    {"reg": 9,  "scale": 0.1,    "unit": "W"},
+    "pv2_power":    {"reg": 10, "scale": 0.1,    "unit": "W"},
+    "daily_energy": {"reg": 11, "scale": 0.005,  "unit": "kWh"},
+    "total_energy": {"reg": 13, "scale": 0.1,  "unit": "kWh"},
+    "device_temp":  {"reg": 14, "scale": 0.1,  "unit": "°C"},
+    "boost_temp":   {"reg": 15, "scale": 0.1,  "unit": "°C"},
+}
