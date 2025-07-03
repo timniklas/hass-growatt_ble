6cf38167a1bbb6ef9874274f415b977b525d147e
timniklas <accounts@tnkoch.de>
2025-07-03 09:27:01 +0200

    Update __init__.py

--- custom_components/growatt_ble/__init__.py
+++ custom_components/growatt_ble/__init__.py
@@ -1,12 +1,16 @@
 from .const import DOMAIN
+from .sensor import GrowattBLECoordinator
 
 async def async_setup_entry(hass, entry):
+    coordinator = GrowattBLECoordinator(hass, entry.data["serial"])
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.data[DOMAIN][entry.entry_id] = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
+    coordinator = hass.data[DOMAIN].pop(entry.entry_id, None)
+    if coordinator is not None:
+        await coordinator.ble.disconnect()
     await hass.config_entries.async_forward_entry_unloads(entry, ["sensor"])
-    hass.data[DOMAIN].pop(entry.entry_id)
     return True
